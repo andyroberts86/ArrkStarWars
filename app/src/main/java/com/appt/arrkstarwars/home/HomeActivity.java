614c4a74a8623b7888b5912d3537eeb9377a4085
Andy <andrew.roberts86@hotmail.co.uk>
2018-06-22 17:07:46 +0100

    Changed attach/detach to create destroy to keep cache

--- app/src/main/java/com/appt/arrkstarwars/home/HomeActivity.java
+++ app/src/main/java/com/appt/arrkstarwars/home/HomeActivity.java
@@ -43,17 +43,13 @@ public class HomeActivity extends AppCompatActivity implements HomePresenter.Vie
         loading = findViewById(R.id.loading);
 
         recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
         presenter.attach(this);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         presenter.detach();
     }
 
